3d84c8609274ad011ab3a0c8a4efa617819b6449
Teles <rmteles@gmail.com>
2016-05-20 23:26:15 -0300

    Initial commit

---
+++ src/main/java/com/business/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.business.controller;
+
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Path;
+
+@Controller
+public class IndexController {
+
+    @Path("/")
+    public void index() {
+
+    }
+
+}
