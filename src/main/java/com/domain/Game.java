7e5744acaafa97a8fab5320b7491f73ebeb65dd3
Rafael Teles <rmteles@gmail.com>
2016-05-22 19:02:29 -0300

    Ending game when hitting rounds limit

--- src/main/java/com/domain/Game.java
+++ src/main/java/com/domain/Game.java
@@ -27,7 +27,7 @@ public class Game implements MasterEntity {
     private String secret;
 
     public static final int DEFAULT_MULTI_PLAYER_LIMIT = 2;
-    public static final int DEFAULT_ROUNDS_LIMIT = 8;
+    public static final int DEFAULT_ROUNDS_LIMIT = Integer.MAX_VALUE;
     public static final int DEFAULT_POSITIONS = 8;
     public static final int DEFAULT_SECRET_SIZE = 8;
     public static final int DEFAULT_COLORS_COUNT = 8;
